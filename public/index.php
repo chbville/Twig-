dc707a4950b0bcffb976749d7a5e2ad4a1c8eba6
chbville <bonneville.charles@yahoo.fr>
2021-10-20 10:51:18 +0200

    Twig

--- public/index.php
+++ public/index.php
@@ -2,8 +2,10 @@
 
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
+require '../src/products.php';
 
 
 $name = 'Wilder';
 
 echo $twig->render('home.html.twig', ['name' => $name]);
+echo $twig->render('products.html.twig', ['products' => $products]);
